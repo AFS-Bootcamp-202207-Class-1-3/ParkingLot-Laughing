2c68e23d2469b2a86bb6748897962b5b38ffe1c8
Laughinggg <953651397@qq.com>
2022-07-22 17:15:44 +0800

    feat: ParkingBoy should_return_corresponding_car_when_fetchCar_given_ticket

--- src/test/java/com/parkinglot/ParkingBoyTest.java
+++ src/test/java/com/parkinglot/ParkingBoyTest.java
@@ -25,4 +25,17 @@ class ParkingBoyTest {
 
         assertNotNull(car);
     }
+
+    @Test
+    public void should_return_corresponding_car_when_fetchCar_given_ticket(){
+        ParkingLot parkingLot=new ParkingLot(10);
+        ParkingBoy parkingBoy=new ParkingBoy(parkingLot);
+        Car car1=new Car("A123");
+        Car car2=new Car("A321");
+        ParkingTicket ticket1=parkingBoy.park(car1);
+        ParkingTicket ticket2=parkingBoy.park(car2);
+
+        assertEquals(car1,parkingBoy.fetchCar(ticket1));
+        assertEquals(car2,parkingBoy.fetchCar(ticket2));
+    }
 }
