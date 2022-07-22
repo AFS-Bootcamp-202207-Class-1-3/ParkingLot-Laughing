966731cf00dd75de05cb65865bfd826814c7b791
Laughinggg <953651397@qq.com>
2022-07-22 11:53:57 +0800

    feat: should_return_null_when_park_given_car_no_position

--- src/main/java/com/parkinglot/ParkingLot.java
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -1,7 +1,28 @@
 package com.parkinglot;
 
 public class ParkingLot {
+
+    private int totalCapacity=10;
+    private int surplusCapacity=10;
+
+    public ParkingLot(int totalCapacity, int surplusCapacity) {
+        this.totalCapacity = totalCapacity;
+        this.surplusCapacity = surplusCapacity;
+    }
+
+    public ParkingLot(int totalCapacity) {
+        this.totalCapacity = totalCapacity;
+        this.surplusCapacity=totalCapacity;
+    }
+
     public ParkingTicket park(Car car) {
-        return new ParkingTicket();
+
+        return isSurplus()?new ParkingTicket():null;
+    }
+
+
+
+    private boolean isSurplus(){
+        return surplusCapacity>0;
     }
 }
--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -2,17 +2,28 @@ package com.parkinglot;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class ParkingLotTest {
 
     @Test
     public void should_return_parking_ticket_when_park_given_a_car(){
         Car car=new Car();
-        ParkingLot parkingLot=new ParkingLot();
+        ParkingLot parkingLot=new ParkingLot(10);
 
         ParkingTicket parkingTicket=parkingLot.park(car);
 
         assertNotNull(parkingTicket);
+    }
+
+    @Test
+    public void should_return_null_when_park_given_car_no_position(){
+        Car car=new Car();
+        ParkingLot parkingLot=new ParkingLot(10,0);
+
+        ParkingTicket parkingTicket=parkingLot.park(car);
+
+        assertNull(parkingTicket);
     }
 }
